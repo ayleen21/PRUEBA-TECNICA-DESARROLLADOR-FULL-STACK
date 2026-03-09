b43170d363d33c09e277ab03ea967d4465bd398c
ayleen21 <ayleenmurillo77@gmail.com>
2026-03-09 18:47:34 -0500

    feat(api): se añade configuracion del CORS

--- BackendFacturas/Program.cs
+++ BackendFacturas/Program.cs
@@ -4,7 +4,17 @@ using BackendFacturas.Services;
 using Swashbuckle.AspNetCore;
 
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuración de CORS para desarrollo
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowFrontend",
+		policy => policy.WithOrigins("http://localhost:3000")
+						.AllowAnyHeader()
+						.AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,12 +22,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
 builder.Services.AddScoped<InvoiceService>();
 
+
 var app = builder.Build();
+
 
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+	app.UseCors("AllowFrontend");
 }
 
 app.MapControllers();
