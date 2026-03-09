485fd780520f2af87c7be0073f15e030d34c004f
ayleen21 <ayleenmurillo77@gmail.com>
2026-03-09 17:11:50 -0500

    feat(models): crear entidad de invoice(factura)

---
+++ BackendFacturas/Models/Invoice.cs
@@ -0,0 +1,10 @@
+namespace BackendInvoices.Models;
+
+public class Invoice
+{
+    public int InvoiceID { get; set; }
+    public DateTime Date { get; set; }
+    public decimal Amount { get; set; }
+    public string Status { get; set; }
+    public int CustomerID { get; set; }
+}
