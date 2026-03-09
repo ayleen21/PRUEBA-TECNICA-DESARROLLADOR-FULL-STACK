b619e45b4c21fb201f8884fff42b2777f58ef9a5
ayleen21 <ayleenmurillo77@gmail.com>
2026-03-09 17:24:19 -0500

    refactor(models): Se corrige namespace

--- BackendFacturas/Models/Invoice.cs
+++ BackendFacturas/Models/Invoice.cs
@@ -1,10 +1,10 @@
-namespace BackendInvoices.Models;
+namespace BackendFacturas.Models;
 
 public class Invoice
 {
     public int InvoiceID { get; set; }
     public DateTime Date { get; set; }
     public decimal Amount { get; set; }
-    public string Status { get; set; }
+    public string? Status { get; set; }
     public int CustomerID { get; set; }
 }
